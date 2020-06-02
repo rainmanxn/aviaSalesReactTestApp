20c9f1061a3f9c9a0723eba2577b3e504897c197
rainmanxn <osryazantsev@gmail.com>
2020-06-02 16:55:23 +0300

    02.06.20

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import axios from 'axios';
+import { format, compareAsc } from 'date-fns';
+import Card from './Card';
 
 export default class App extends React.Component {
   state = {
     url: 'https://front-test.beta.aviasales.ru/tickets?searchId=',
-  }
+  };
 
   async componentDidMount() {
     const response = await axios.get('https://front-test.beta.aviasales.ru/search');
     console.log(response.data);
     const { searchId } = response.data;
     console.log(searchId);
-    const tickets = await axios.get(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`);
+    const tickets = await axios.get(
+      `https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`
+    );
     console.log(tickets.data);
+    const tickets1 = await axios.get(
+      `https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`
+    );
+    console.log(tickets1.data);
+    const data1 = format(new Date(2014, 1, 11), 'MM/dd/yyyy');
+    console.log(data1);
   }
 
   render() {
-    return <div>Res</div>;
+    return (
+      <>
+        <div>Res</div>
+        <Card />
+      </>
+    );
   }
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+  width: 502px;
+  height: 184px;
+  background: #ffffff;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+`;
+const Header = styled.div`
+  position: absolute;
+  width: 432px;
+  height: 36px;
+  left: 20px;
+  top: 26px;
+  border: 1px solid black;
+`;
+
+const Price = styled.div`
+  position: absolute;
+  top: 18.96%;
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  color: #2196f3;
+`;
+
+class Card extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Price>13400 р</Price>
+        </Header>
+      </Container>
+    );
+  }
+}
+
+export default Card;
