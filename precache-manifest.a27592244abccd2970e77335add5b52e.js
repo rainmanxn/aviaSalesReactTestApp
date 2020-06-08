3f903849e22b26231a1793fcc4565defccdff21f
rainmanxn <osryazantsev@gmail.com>
2020-06-08 22:49:54 +0300

    Updates

---
+++ precache-manifest.a27592244abccd2970e77335add5b52e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f0f76fba6d57066bcdedaafdbdac12",
+    "url": "/avia19/index.html"
+  },
+  {
+    "revision": "13019f7964c70d943785",
+    "url": "/avia19/static/css/main.0fa0cef4.chunk.css"
+  },
+  {
+    "revision": "51c686b6dec12210097d",
+    "url": "/avia19/static/js/2.894d8534.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/avia19/static/js/2.894d8534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13019f7964c70d943785",
+    "url": "/avia19/static/js/main.5171c995.chunk.js"
+  },
+  {
+    "revision": "55043155db108cd31158",
+    "url": "/avia19/static/js/runtime-main.737a8b48.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/avia19/static/media/logo.0c089234.svg"
+  }
+]);
